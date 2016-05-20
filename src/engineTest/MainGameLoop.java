0b1c96cf10855ee92ccc5e32dcfb066c46b1a0d8
sherine sameh <sherine-sameh@hotmail.com>
2016-05-20 19:20:04 +0200

    add shader to main game loop

--- src/engineTest/MainGameLoop.java
+++ src/engineTest/MainGameLoop.java
@@ -1,7 +1,8 @@
 package engineTest;
 import renderEngine.*;
+import org.lwjgl.opengl.*;
+import shader.*;
 
-import org.lwjgl.opengl.Display;
 public class MainGameLoop {
 	
 	public static void main(String[] args)
@@ -9,24 +10,30 @@ public class MainGameLoop {
 	DisplayManager.createDisplay();
 	Loader loader = new Loader();
 	Renderer renderer = new Renderer();
+        StaticShader shader =new StaticShader();
 	float[] vertices =
 		{
 			-0.5f,0.5f,0f, //V0
 			-0.5f,-0.5f,0f, //V1
-			-0.5f,-0.5f,0f, //V2
+			0.5f,-0.5f,0f, //V2
 			0.5f,0.5f,0f,  //V3
 		};
 	int [] indices = 
 		{
-		0,1,3,3,1,2
+		0,1,3,
+                3,1,2
 		};
 	RawModel model = loader.loadToVAO(vertices,indices);
 	while(!Display.isCloseRequested())
 	{
 		renderer.prepare();
+                shader.start();
 		renderer.render(model);
+                shader.stop();
 		DisplayManager.updateDisplay();
-	}
+
+        }
+        shader.cleanUp();
 	loader.cleanUp();
 	
 	DisplayManager.closeDisplay();
